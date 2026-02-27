c034e66d570b33645715d64d12a43e6ff1255ff9
chachacollins <collinschacha@hotmail.com>
2026-02-27 17:00:29 +0300

    feat: extract hostname from url

---
+++ geminiclient.c
@@ -0,0 +1,130 @@
+// Based on ssl example by Alexey Kutepov <reximkut@gmail.com>
+
+#define _POSIX_C_SOURCE 200112L
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <stdbool.h>
+
+#include <netdb.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+
+#define HOST "geminiprotocol.net"
+#define PORT "1965"
+
+// gemini://geminiprotocol.net/
+#define PROTOCAL "gemini://"
+
+char* get_hostname(char *url)
+{
+    size_t prot_len = strlen(PROTOCAL);
+    if(strncmp(url, PROTOCAL, prot_len) != 0)
+    {
+        fprintf(stderr, 
+                "ERROR: malformed request url provided: no begining protocal %s\n",
+                url
+               );
+        return NULL;
+    }
+    char* begin_host = url+prot_len;
+    char* end_host = strchr(begin_host, '/');
+    if(end_host == NULL)
+    {
+        fprintf(stderr, 
+                "ERROR: malformed request url provided: no terminating slash %s\n",
+                url
+               );
+        return NULL;
+    }
+    size_t len = end_host - begin_host;
+    return strndup(begin_host, len);
+}
+
+int main(int argc, char **argv)
+{
+    if(argc < 2)
+    {
+        fprintf(stderr, "ERROR: please provide a url\n");
+        return 1;
+    }
+    char *url = argv[1];
+    char* hostname = get_hostname(url);
+    struct addrinfo hints = {0};
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+
+    struct addrinfo *addrs;
+    if (getaddrinfo(hostname, PORT, &hints, &addrs) < 0) {
+        fprintf(stderr, "Could not get address of `%s`: %s\n",
+                hostname,
+                strerror(errno));
+        free(hostname);
+        exit(1);
+    }
+    free(hostname);
+
+    int sd = 0;
+    for (struct addrinfo *addr = addrs; addr != NULL; addr = addr->ai_next) {
+        sd = socket(addr->ai_family, addr->ai_socktype, addr->ai_protocol);
+
+        if (sd == -1) break;
+        if (connect(sd, addr->ai_addr, addr->ai_addrlen) == 0) break;
+
+        close(sd);
+        sd = -1;
+    }
+    freeaddrinfo(addrs);
+
+    if (sd == -1) {
+        fprintf(stderr, "Could not connect to "HOST":"PORT": %s\n",
+                strerror(errno));
+        exit(1);
+    }
+
+    OpenSSL_add_all_algorithms();
+    SSL_load_error_strings();
+    SSL_CTX *ctx = SSL_CTX_new(TLS_client_method());
+
+    if (ctx == NULL) {
+        fprintf(stderr, "ERROR: could not initialize the SSL context: %s\n",
+                strerror(errno));
+        exit(1);
+    }
+
+    SSL *ssl = SSL_new(ctx);
+    SSL_set_fd(ssl, sd);
+
+    if (SSL_connect(ssl) < 0) {
+        fprintf(stderr, "ERROR: could not connect via SSL: %s\n",
+                strerror(errno));
+        exit(1);
+    }
+    char request[1024] = {0};
+    sprintf(request, "%s\r\n", url);
+    SSL_write(ssl, request, strlen(request));
+
+    char buffer[1024];
+    ssize_t n = SSL_read(ssl, buffer, sizeof(buffer));
+    while (n > 0) {
+        fwrite(buffer, 1, n, stdout);
+        n = SSL_read(ssl, buffer, sizeof(buffer));
+    }
+
+    SSL_set_shutdown(ssl, SSL_RECEIVED_SHUTDOWN | SSL_SENT_SHUTDOWN);
+    SSL_shutdown(ssl);
+    SSL_free(ssl);
+    SSL_CTX_free(ctx);
+    close(sd);
+    return 0;
+}
