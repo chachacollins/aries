fa8c7f68361cce41b2365aa8d63b297ad5bb9fee
chachacollins <collinschacha@hotmail.com>
2026-03-04 11:41:43 +0300

    feat: minimal styling of ui

--- src/ui.rs
+++ src/ui.rs
@@ -1,8 +1,7 @@
 use crate::aries_logo;
 use crate::gemini;
-use tui_input::Input;
-use tui_input::backend::crossterm::EventHandler;
 use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind, KeyModifiers};
+use openssl::ssl::SslConnector;
 use ratatui::{
     DefaultTerminal, Frame,
     buffer::Buffer,
@@ -12,9 +11,10 @@ use ratatui::{
     widgets::{Block, Borders, Clear, Paragraph, Widget, Wrap},
 };
 use std::io;
-use openssl::ssl::SslConnector;
+use tui_input::Input;
+use tui_input::backend::crossterm::EventHandler;
 
-#[derive(Debug,PartialEq)]
+#[derive(Debug, PartialEq)]
 enum Page {
     Title,
     Browse,
@@ -22,6 +22,12 @@ enum Page {
 
 const BG_COLOR: Color = Color::Rgb(10, 10, 16);
 const FG_COLOR: Color = Color::Rgb(235, 219, 178);
+const HEADING1_COLOR: Color = Color::Rgb(234, 105, 98);
+const HEADING2_COLOR: Color = Color::Rgb(216, 166, 87);
+const HEADING3_COLOR: Color = Color::Rgb(125, 174, 163);
+const LINK_COLOR: Color = Color::Rgb(137, 180, 130);
+const QUOTE_COLOR: Color = Color::Rgb(146, 131, 116);
+const LIST_COLOR: Color = Color::Rgb(130, 131, 116);
 
 
 fn centered_rect(percent_x: u16, percent_y: u16, r: Rect) -> Rect {
@@ -54,7 +60,7 @@ enum InputMode {
 struct Url {
     input: Input,
     input_mode: InputMode,
-    input_area:Rect,
+    input_area: Rect,
 }
 
 #[derive(Debug)]
@@ -65,7 +71,7 @@ pub struct App {
     event: Event,
     exit: bool,
     url: Url,
-    page_content: String,
+    page_content: Vec<gemini::LineType>,
 }
 
 impl App {
@@ -77,13 +83,13 @@ impl App {
             event: Event::FocusLost,
             url: Url::default(),
             exit: false,
-            page_content: String::new(),
+            page_content: Vec::new(),
         }
     }
 
     pub fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
         while !self.exit {
-            terminal.draw(|frame|{ 
+            terminal.draw(|frame| {
                 self.calculate_url_input_size(frame.area());
                 self.draw(frame)
             })?;
@@ -153,9 +159,11 @@ impl App {
     fn make_request(&mut self) {
         let url = self.url.input.value_and_reset();
         self.stop_editing();
-        self.page_content.clear();
         //TODO: remove this unwrap and report the error to the user
-        self.page_content = gemini::make_request(&self.ssl_connection, &url).unwrap();
+        let res = gemini::make_request(&self.ssl_connection, &url).unwrap();
+        let mut parser = gemini::Parser::new(&res);
+        parser.parse_gemtext();
+        self.page_content = parser.get_lines();
         self.current_page = Page::Browse;
     }
 
@@ -165,7 +173,7 @@ impl App {
 
     fn calculate_url_input_size(&mut self, area: Rect) {
         let width = (area.width as f32 * 0.80) as u16;
-        let x = (area.width - width)/2;
+        let x = (area.width - width) / 2;
         self.url.input_area = Rect {
             y: 1,
             x,
@@ -195,11 +203,10 @@ impl App {
             .border_style(Style::default().fg(Color::Rgb(137, 180, 130)))
             .style(Style::default().bg(BG_COLOR))
             .render(area, buf);
-        Paragraph::new(logo)
-            .centered().render(vertical_center, buf);
+        Paragraph::new(logo).centered().render(vertical_center, buf);
         let help = Line::from(Span::styled(
             "Press <Ctrl-h> to view the help",
-            Style::default().fg(Color::Rgb(197,197,197)),
+            Style::default().fg(Color::Rgb(197, 197, 197)),
         ));
         let vertical_bottom = Rect {
             y: area.y + area.height - 2,
@@ -210,15 +217,54 @@ impl App {
         Paragraph::new(help).render(vertical_bottom, buf);
     }
 
+    fn style_heading(&self, heading: &gemini::Heading) -> Line<'_> {
+        match heading.level {
+            1 => Line::from(Span::styled(heading.title.clone(), Style::default().fg(HEADING1_COLOR))),
+            2 => Line::from(Span::styled(heading.title.clone(), Style::default().fg(HEADING2_COLOR))),
+            3 => Line::from(Span::styled(heading.title.clone(), Style::default().fg(HEADING3_COLOR))),
+            _ => unreachable!(),
+        }
+    }
+
+    fn style_link(&self, link: String) -> Line<'_> {
+        Line::from(Span::styled(link, Style::default().fg(LINK_COLOR)))
+    }
+
+    fn style_quote(&self, quote: String) -> Line<'_> {
+        Line::from(Span::styled(quote, Style::default().fg(QUOTE_COLOR)))
+    }
+
+    fn style_text(&self, text: String) -> Line<'_> {
+        Line::from(Span::styled(text, Style::default().fg(FG_COLOR)))
+    }
+
+    fn style_list(&self, list: String) -> Line<'_> {
+        Line::from(Span::styled(list, Style::default().fg(LIST_COLOR)))
+    }
+
+    fn style_line_types(&self) -> Vec<Line<'_>> {
+        use gemini::LineType::*;
+        let mut lines = vec![];
+        for line in self.page_content.iter() {
+            match line {
+                Heading(heading) => lines.push(self.style_heading(heading)),
+                Link(link) => lines.push(self.style_link(link.to_string())),
+                List(list) => lines.push(self.style_list(list.to_string())),
+                Quote(quote) => lines.push(self.style_quote(quote.to_string())),
+                Preformat(preformated) => {
+                    for formated in preformated {
+                        lines.push(Line::from(formated.to_string()))
+                    }
+                }
+                Text(text) => lines.push(self.style_text(text.to_string())),
+            }
+        }
+        lines
+    }
+
     fn render_browse_page(&self, area: Rect, buf: &mut Buffer) {
         buf.set_style(area, Style::default().bg(BG_COLOR));
-        let mut lines = vec![];
-        for line in self.page_content.lines() {
-            lines.push(Line::from(Span::styled(
-                line,
-                Style::default().fg(FG_COLOR),
-            )));
-        }
+        let lines = self.style_line_types(); 
         let text_height = lines.len() as u16;
         let text = Text::from(lines);
         Block::default()
@@ -232,7 +278,7 @@ impl App {
             width: area.width - 3,
             ..area
         };
-        //TODO: add scrolling using vim motions
+        ////TODO: add scrolling using vim motions
         Paragraph::new(text)
             .wrap(Wrap { trim: true })
             .render(area, buf);
